b06c0d47b72c9b1a60649fb36fc75638b29c73b7
Kaishiro Mitsui <kamitsui@c1r21s1.42tokyo.jp>
2023-01-31 13:58:40 +0900

    itoa ok

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,37 @@
 /*   By: kamitsui <kamitsui@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 20:39:49 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/01/30 22:51:42 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/01/31 13:56:41 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+static int	ft_itoa_len(long n_long)
+{
+	int		len;
+
+	len = 0;
+	if (n_long == 0)
+		len = 1;
+	else
+	{
+		while (n_long > 0)
+		{
+			n_long /= 10;
+			len++;
+		}
+	}
+	return (len);
+}
+
+static int	ft_itoa_sign(int n)
+{
+	if (n >= 0)
+		return (1);
+	else
+		return (-1);
+}
 
 char	*ft_itoa(int n)
 {
@@ -20,24 +46,18 @@ char	*ft_itoa(int n)
 	long	n_long;
 
 	n_long = n;
-	if (n >= 0)
-		sign = 1;
-	else
-		sign = -1;
-	n_long = n * sign;
-	len = 0;
-	while ((n_long /= 10) > 0)
-		len++;
-	str = (char *)malloc(sizeof(char *) * (len + (sign < 0) + 1));
+	sign = ft_itoa_sign(n);
+	n_long *= sign;
+	len = ft_itoa_len(n_long);
+	str = (char *)malloc(sizeof(char) * (len + (sign < 0) + 1));
 	if (str == NULL)
 		return (NULL);
 	if (sign < 0)
 		str[0] = '-';
-	str[len + (sign < 0) +1] = '\0';
-	n_long = n * sign;
-	while (len + 1)
+	str[len + 1 - (sign > 0)] = '\0';
+	while (len)
 	{
-		str[len + (sign < 0)] = n_long % 10 + '0';
+		str[len - (sign > 0)] = n_long % 10 + '0';
 		n_long /= 10;
 		len--;
 	}
